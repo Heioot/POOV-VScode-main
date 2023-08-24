e4691f7c613c429aad80b3460f995fa175367788
Heioot <heitorluissmelo@gmail.com>
2023-08-24 10:54:47 -0300

    teste

--- Lampada/src/App.java
+++ Lampada/src/App.java
@@ -9,15 +9,20 @@ public class App {
         l1.tensao = 110;
         l1.potencia = 100;
         l1.acesa = true;
+        l1.quebrada = true;
+        l1.queimada = true;
         l1.mostrar();
 
-        Lampada l2 = new Lampada();
+        Lampada l2 = new Lampada(220,60);
 
-        l2.tensao = 220;
-        l2.potencia = 60;
-        l2.acesa = false;
+        // l2.tensao = 220;
+        // l2.potencia = 60;
         l2.quebrada = true;
         l2.mostrar();
+        l2.acesa = false;
+
+        Lampada l3 = new Lampada(110,60,true);
+        l3.mostrar();
 
     }
 
