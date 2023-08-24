907a3ef64b93b0dda5a120e2ac20283d16b57c21
Heioot <heitorluissmelo@gmail.com>
2023-08-24 10:54:54 -0300

    teste

--- Lampada/src/Lampada.java
+++ Lampada/src/Lampada.java
@@ -1,11 +1,48 @@
 
 public class Lampada {
     
-    public int tensao; // 110, 220
-    public int potencia; 
-    public boolean acesa;
-    public boolean quebrada;
-    public boolean queimada;
+    private int tensao; // 110, 220
+    private int potencia; 
+    private boolean acesa;
+    private boolean quebrada;
+    private boolean queimada;
+
+
+// por padrão boolean inicia com false.
+
+// Construtor padrão = public Lampada(){
+
+
+
+
+// }
+
+    public Lampada(){
+        System.out.println("Construtor padrão da lampada executado");
+        tensao = 220;
+        potencia = 100;
+        //acesa = false;
+    }
+
+
+    public Lampada(int t, int p ){
+        System.out.println("Construtor padrão da lampada com dois parâmetros iniciados");
+        tensao = t;
+        potencia = p;
+    }
+
+
+
+    public Lampada(int t, int p, boolean a){
+        tensao = t;
+        potencia = p;
+        acesa = a;
+    }
+
+
+
+
+
 
     public void mostrar() {
         String texto = tensao + "V, " + potencia + "W, " +((acesa) ? "Acesa" : "Apagada")
