0f02caa1bec4d0ac13894bf1cbd4e034d0ab099a
Heioot <heitorluissmelo@gmail.com>
2023-08-29 09:42:20 -0300

    get  e set

--- Lampada/src/App.java
+++ Lampada/src/App.java
@@ -6,23 +6,30 @@ public class App {
         Lampada l1 = new Lampada();
         l1.mostrar();
 
-        l1.tensao = 110;
-        l1.potencia = 100;
-        l1.acesa = true;
-        l1.quebrada = true;
-        l1.queimada = true;
+        //l1.tensao = 110;
+        l1.setTensao(110);
+        //l1.potencia = 100;
+        l1.setPotencia(60);
+        //l1.acesa = true;
+        l1.quebrar();
+        //l1.setAcesa(true);
+        l1.acender();
         l1.mostrar();
 
         Lampada l2 = new Lampada(220,60);
 
         // l2.tensao = 220;
         // l2.potencia = 60;
-        l2.quebrada = true;
+        //l2.quebrada = true;
+        l2.setQuebrada(true);
         l2.mostrar();
-        l2.acesa = false;
 
         Lampada l3 = new Lampada(110,60,true);
         l3.mostrar();
+
+        //Total de potencia gasta pelas 3 lampadas:
+
+        System.out.println("Soma das potencias:" + (l1.getPotencia() + l2.getPotencia() + l3.getPotencia()));
 
     }
 
--- Lampada/src/Lampada.java
+++ Lampada/src/Lampada.java
@@ -1,3 +1,5 @@
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
 
 public class Lampada {
     
@@ -16,33 +18,117 @@ public class Lampada {
 
 
 // }
-
+//construtor
     public Lampada(){
+        this(110,100);
         System.out.println("Construtor padrão da lampada executado");
-        tensao = 220;
-        potencia = 100;
+        //tensao = 220;
+        //potencia = 100;
         //acesa = false;
     }
 
-
-    public Lampada(int t, int p ){
+//construtor
+    public Lampada(int tensao, int potencia ){
+        this(tensao, potencia, false);
         System.out.println("Construtor padrão da lampada com dois parâmetros iniciados");
-        tensao = t;
-        potencia = p;
+        //tensao = t;
+        setTensao(tensao); // PARA ATRIBUIR UM VALOR A TENSAO SEGUINDO REGRAS DO MÉTODO SETTENSAO
+        //potencia = p;
+        setPotencia(potencia); // PARA ATRIBUIR UM VLAOR A TENSAO SEGUINDO REGRAS DO MÉTODO SETPOTENCIA
     }
 
 
-
-    public Lampada(int t, int p, boolean a){
-        tensao = t;
-        potencia = p;
-        acesa = a;
+//construtor
+    public Lampada(int t, int p, boolean acesa){
+        setTensao(tensao);
+        setPotencia(potencia);
+        this.acesa = acesa;
     }
 
+public void setTensao(int tensao){
+    if ((tensao == 110) || (tensao == 220)){
+        this.tensao = tensao;
+    } else {
+        this.tensao = 220;
+        throw new InvalidParameterException("Valor inválido de parametro");
+    }
+}
+
+public int getTensao(){
+    return tensao;
+}
+
+public void setPotencia(int potencia){
+    if ((potencia == 60) || (potencia == 100 || (potencia == 150))){
+        this.potencia = potencia;
+    } else {
+        this.potencia = 60;
+        throw new InvalidParameterException("Valor inválido de potência");
+    }
+    this.potencia = potencia;
+}
 
 
+public int getPotencia(){
+    return potencia;
+}
+
+public void setAcesa(boolean acesa){
+    if (!queimada && !quebrada){
+        this.acesa = acesa;
+    } else {
+        if (queimada && acesa ){
+            queimada = true;
+            acesa = true;
+        }
+        this.acesa = false;
+    }
+}
+
+public boolean isAcesa(){
+    return acesa;
+}
+
+public void setQuebrada(boolean quebrada){
+    this.quebrada = quebrada;
+}
+
+public boolean isQuebrada(){
+    return quebrada;
+}
 
 
+public void setQueimada(boolean queimada){
+    if (queimada && acesa){
+        acesa = false;
+    }
+    this.queimada = queimada;
+}
+
+public boolean isQueimada(){
+    return queimada;
+}
+
+
+public void acender(){
+    setAcesa(true);
+}
+
+public void apagar(){
+    setAcesa(false);
+}
+
+public void queimar(){
+    setQueimada(true);
+}
+
+public void quebrar(){
+    setQuebrada(true);
+}
+
+//SE FOSSE CRIAR UM ARRAYLIST LAMPADA
+
+    ArrayList<Lampada> vetor = new ArrayList<Lampada>();
 
     public void mostrar() {
         String texto = tensao + "V, " + potencia + "W, " +((acesa) ? "Acesa" : "Apagada")
