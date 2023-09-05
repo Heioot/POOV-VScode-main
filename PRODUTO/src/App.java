e709975799018074c99495ccb118202fc70262f5
Heioot <heitorluissmelo@gmail.com>
2023-09-05 08:17:21 -0300

    teste

--- PRODUTO/src/App.java
+++ PRODUTO/src/App.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        ArrayList<Produto> produtos = new ArrayList<Produto>();
+        
+        Produto p = new Produto();
+
+
+        produtos.add(new Produto());
+        produtos.add(p.obteProduto());
+
+
+        for(Produto produto : produtos){
+            produto.mostrar();
+            System.out.println();
+        }
+
     }
 }
