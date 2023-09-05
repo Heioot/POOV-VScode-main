7502fb6adccaed19e14bec1c44078d94e9bf111a
Heioot <heitorluissmelo@gmail.com>
2023-09-05 08:04:54 -0300

    tawrwarawrar

--- PRODUTO/src/Produto.java
+++ PRODUTO/src/Produto.java
@@ -1,87 +1,174 @@
 import java.math.BigDecimal;
-import java.security.InvalidAlgorithmParameterException;
 import java.time.LocalDate;
-import java.security.InvalidParameterException;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class Produto {
+
     private long codigo;
     private String nome;
     private String descricao;
-    private BigDecimal precocompra;
-    private LocalDate datacompra;
-    private LocalDate datavalidade;
-    private int situacao;
     private String marca;
-    private int quantminima;
-    private int quantatual;
+    private BigDecimal precoCompra; // null
+    private LocalDate dataCompra;
+    private LocalDate dataValidade;
+    private int quantMinima;
+    private int quantAtual;
+    private int situacao;
 
-
-
-    // contrutores
-
-    public Produto(){
+    public Produto() {
+        codigo = 0L;
+        nome = "sem nome";
+        descricao = "sem descricao";
+        marca = "sem marca";
+        precoCompra = BigDecimal.ZERO; // new BigDecimal(0);
+        dataCompra = LocalDate.now();
+        dataValidade = LocalDate.now();
+        quantMinima = quantAtual = 0;
+        situacao = 0;
     }
 
-
-    public Produto(long codigo, String nome, String descricao, BigDecimal precocompra, LocalDate datacompra, int situacao, String marca, int quantminima, int quantatual){
-
+    public Produto(long codigo, String nome, String descricao, String marca, BigDecimal precoCompra,
+            LocalDate dataCompra, LocalDate dataValidade, int quantMinima, int quantAtual, int situacao) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.marca = marca;
+        this.precoCompra = precoCompra;
+        this.dataCompra = dataCompra;
+        this.dataValidade = dataValidade;
+        this.quantMinima = quantMinima;
+        this.quantAtual = quantAtual;
+        this.situacao = situacao;
     }
- // set e get 
 
-
-    public void setCodigo(){
-        if (codigo > 0){
-            throw new IllegalArgumentException();
-        } else {
-            this.codigo = codigo;
-        }
-    }
-
-    public long getCodigo(long codigo){
+    public long getCodigo() {
         return codigo;
     }
 
-
-    public void setNome(){
-        this.nome = nome;
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
     }
 
-    public String getNome(String Nome){
+    public String getNome() {
         return nome;
     }
 
-    public void setDescricao(){
-        this.descricao = descricao;
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
-    public String getDescricao(String descricao){
+    public String getDescricao() {
         return descricao;
     }
 
-    public void precocompra(){
-        if(precocompra.compareTo(BigDecimal.ZERO) >= 0){
-            this.precocompra = precocompra;
-        }else{
-            throw new IllegalArgumentException();
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
     }
 
-    }
-    
-
-    public BigDecimal getPrecocompra(){
-        return precocompra;
+    public String getMarca() {
+        return marca;
     }
 
-    public void setDatacompra(){
-        this.datacompra = LocalDate.now();
+    public void setMarca(String marca) {
+        this.marca = marca;
     }
 
-    public boole setDatavalidade(){
-        LocalDate hoje;
-        hoje = LocalDate.now();
-        return datavalidade.isBefore(hoje);
+    public BigDecimal getPrecoCompra() {
+        return precoCompra;
     }
 
-    public 
+    public void setPrecoCompra(BigDecimal precoCompra) {
+        this.precoCompra = precoCompra;
+    }
+
+    public LocalDate getDataCompra() {
+        return dataCompra;
+    }
+
+    public void setDataCompra(LocalDate dataCompra) {
+        this.dataCompra = dataCompra;
+    }
+
+    public LocalDate getDataValidade() {
+        return dataValidade;
+    }
+
+    public void setDataValidade(LocalDate dataValidade) {
+        this.dataValidade = dataValidade;
+    }
+
+    public int getQuantMinima() {
+        return quantMinima;
+    }
+
+    public void setQuantMinima(int quantMinima) {
+        this.quantMinima = quantMinima;
+    }
+
+    public int getQuantAtual() {
+        return quantAtual;
+    }
+
+    public void setQuantAtual(int quantAtual) {
+        this.quantAtual = quantAtual;
+    }
+
+    public int getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(int situacao) {
+        this.situacao = situacao;
+    }
+
+    public boolean isEmFalta() {
+        return quantAtual < quantMinima;
+    }
+
+    public boolean isVencido() {
+        return LocalDate.now().isAfter(dataValidade);
+    }
+
+    public Produto obteProduto() {
+        Produto novo = new Produto();
+        Scanner s = new Scanner(System.in);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        System.out.print("Digite o codigo: ");
+        novo.setCodigo(Long.parseLong(s.nextLine()));
+        System.out.print("Digite o nome: ");
+        novo.setNome(s.nextLine());
+        System.out.print("Digite a descricao: ");
+        novo.setDescricao(s.nextLine());
+        System.out.print("Digite a marca: ");
+        novo.setMarca(s.nextLine());
+        System.out.print("Digite o preco de compra: ");
+        novo.setPrecoCompra(new BigDecimal(s.nextLine()));
+        System.out.print("Digite a data da compra (DD/MM/AAAA): ");
+        novo.setDataCompra(LocalDate.parse(s.nextLine(), dtf));
+        System.out.print("Digite a data de validade (DD/MM/AAAA): ");
+        novo.setDataValidade(LocalDate.parse(s.nextLine(), dtf));
+        System.out.print("Quantidade minima: ");
+        novo.setQuantMinima(Integer.parseInt(s.nextLine()));
+        System.out.print("Quantidade atual: ");
+        novo.setQuantAtual(Integer.parseInt(s.nextLine()));
+        System.out.print("Situacao: ");
+        novo.setSituacao(Integer.parseInt(s.nextLine()));
+        s.close();
+        return novo;
+    }
+
+    public void mostrar() {
+        System.out.println("Codigo: " + codigo);
+        System.out.println("Nome: " + nome);
+        System.out.println("Descricao: " + descricao);
+        System.out.println("Marca: " + marca);
+        System.out.println("Preco de compra: " + precoCompra);
+        System.out.println("Data da compra: " + dataCompra);
+        System.out.println("Data de validade: " + dataValidade);
+        System.out.println("Quantidade minima: " + quantMinima);
+        System.out.println("Quantidade atual: " + quantAtual);
+        System.out.println("Situacao: " + situacao);
+    }
 
 }
