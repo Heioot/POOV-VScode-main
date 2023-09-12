344465031ae1ce8bd3247b9cff1b0624ba6076c5
Heioot <heitorluissmelo@gmail.com>
2023-09-12 08:26:51 -0300

    teste

--- Lampada/src/poov/App.java
+++ Lampada/src/poov/App.java
@@ -3,45 +3,67 @@ package poov;
 public class App {
 
     public static void main(String[] args) throws Exception {
-
-
-        LampadaColorida lc = new LampadaColorida();
-
-        lc.mostrar();
-        lc.setCor("azul");
-        lc.mostrar();
-
-
-
-
         
+        LampadaColorida lc = new LampadaColorida();
+        // // lc.mostrar();
+        lc.setCor("Azul");
+        lc.mostrar();
+
+        lc.toString();
+
+        // System.out.println(lc);
+
         Lampada l1 = new Lampada();
-        l1.mostrar();
+        System.out.println(l1);
 
-        //l1.tensao = 110;
-        l1.setTensao(110);
-        //l1.potencia = 100;
-        l1.setPotencia(60);
-        //l1.acesa = true;
-        l1.quebrar();
-        //l1.setAcesa(true);
-        l1.acender();
-        l1.mostrar();
+        Lampada l2 = new Lampada();
+        System.out.println(l2);
 
-        Lampada l2 = new Lampada(220,60);
+        if (l1 == l2) { 
+            System.out.println("iguais");
+        } else {
+            System.out.println("diferentes");
+        }
 
-        // l2.tensao = 220;
-        // l2.potencia = 60;
-        //l2.quebrada = true;
-        l2.setQuebrada(true);
-        l2.mostrar();
+        if (l1.equals(l2)) {
+            System.out.println("iguais");
+        } else {
+            System.out.println("diferentes");
+        }
 
-        Lampada l3 = new Lampada(110,60,true);
-        l3.mostrar();
+        if (l1.equals(l1)) {
+            System.out.println("iguais");
+        } else {
+            System.out.println("diferentes");
+        }
 
-        //Total de potencia gasta pelas 3 lampadas:
+        //Lampada l3 = new Lampada(l1);
+        
 
-        System.out.println("Soma das potencias:" + (l1.getPotencia() + l2.getPotencia() + l3.getPotencia()));
+        // System.out.println();
+        // Lampada l2 = new Lampada(110, 100);
+        // System.out.println();
+        // Lampada l3 = new Lampada(220, 60, true);
+        // l1.mostrar();
+
+        // l1.setTensao(220);
+        // l1.setPotencia(150);
+        // // l1.setAcesa(true);
+        // l1.quebrar();
+        // // l1.setQueimada(true);
+        // l1.acender();
+        // l1.mostrar();
+
+        // Lampada l2 = new Lampada(220, 60);
+        // l2.quebrar();
+        // l2.mostrar();
+
+        // Lampada l3 = new Lampada(110, 60, true);
+        // l3.mostrar();
+
+
+        // //Total de potencia gasta pelas 3 lampadas
+        // System.out.println(l1.getPotencia() + l2.getPotencia() + l3.getPotencia());
 
     }
 
--- Lampada/src/poov/LampadaColorida.java
+++ Lampada/src/poov/LampadaColorida.java
@@ -1,30 +1,67 @@
 package poov;
 
-
 public class LampadaColorida extends Lampada {
-
+    
     private String cor;
 
-    public String getCor(){
+    public LampadaColorida() {
+        super(220, 60);
+        cor = "Sem cor";
+    }
+
+    public String getCor() {
         return cor;
     }
 
-    public void setCor(String cor){
+    public void setCor(String cor) {
         this.cor = cor;
     }
 
+    // public void setTensao(int tensao) {
+    //     System.out.println("Estraguei tudo");
+    //     // super.setTensao(-1000);
+    // }
 
-
-
-    @Override
+    @Override  //o @Override nao eh obrigatorio, mas eh util
     public void mostrar() {
-        String texto = cor + getTensao() + " V, " + getPotencia() + " W, " +((isAcesa()) ? "Acesa" : "Apagada ")
-                                                         +((isQueimada()) ? " ,Queimada" : "")
-                                                         +((isQueimada()) ? " ,Quebrada" : "");
-
+        // super.mostrar();
+        // System.out.println(cor);
+        String texto = cor + ", " + getTensao() + "V, " + getPotencia() + "W, " + ((isAcesa()) ? "Acesa" : "Apagada")
+                + ((isQueimada()) ? ", Queimada" : "")
+                + ((isQuebrada()) ? ", Quebrada" : "");
         System.out.println(texto);
     }
 
+    @Override
+    public String toString() {
+        return super.toString() + ", " + cor;
+    }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((cor == null) ? 0 : cor.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        LampadaColorida other = (LampadaColorida) obj;
+        if (cor == null) {
+            if (other.cor != null)
+                return false;
+        } else if (!cor.equals(other.cor))
+            return false;
+        return true;
+    }
+
+    
 
 }
